3924034a92282fe157d37623464e073b6b9536fe
tarantot <roskishnyi@gmail.com>
2022-07-18 20:40:36 -0600

    All labs and assignments about JS

---
+++ challenge-has-unique-char-solution/hasUniqueChars.js
@@ -0,0 +1,8 @@
+// Write your code below
+function hasUniqueChars(word) {
+  let uniqueChars = new Set([])
+  for (let i = 0; i < word.length; i++) {
+    uniqueChars.add(word[i])
+  }
+  return uniqueChars.size === word.length
+}
